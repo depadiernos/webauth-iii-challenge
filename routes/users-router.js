5de71959a16a352285e8cd3cab74782f0bd01125
Dennis Padiernos <depadiernos@gmail.com>
2020-01-21 21:22:43 -0600

    fix find filter

--- routes/users-router.js
+++ routes/users-router.js
@@ -5,7 +5,7 @@ const router = express.Router()
 
 router.get("/", async (req, res, next) => {
   try {
-    const users = await usersModel.find(req.user.department)
+    const users = await usersModel.find({ department: req.user.department })
     res.json(users)
   } catch (err) {
     next(err)
